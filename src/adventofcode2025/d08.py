151780664df99f8294f563a4052d289c0917a6ba
sbirnb <birnbaum.sam@gmail.com>
2025-12-08 21:22:56 +0100

    Day 8 part 2 a little better

--- src/adventofcode2025/d08.py
+++ src/adventofcode2025/d08.py
@@ -1,7 +1,7 @@
 import heapq
 import sys
 from collections import defaultdict
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Collection
 from heapq import nsmallest, nlargest
 from itertools import combinations
 
@@ -41,36 +41,40 @@ def part1(input_: Iterable[str]) -> int:
 
 
 def part2(input_: Iterable[str]) -> int:
-    edges = list((dist3(p1, p2), p1, p2) for p1, p2 in combinations(parse_input(input_), 2))
+    points = list(parse_input(input_))
+    edges = list((dist3(p1, p2), p1, p2) for p1, p2 in combinations(points, 2))
     heapq.heapify(edges)
 
-    cluster_map = dict()
+    adjacent_map = defaultdict(list)
 
-    clusters = dict()
-    n_clusters = 0
+    def get_cluster(p0: Tuple[int, int, int]) -> Iterable[Tuple[int, int, int]]:
+        if p0 not in adjacent_map:
+            return [p0]
+        cluster = {p0}
+        queue = [p0]
+        while queue:
+            p = queue.pop()
+            for p1 in adjacent_map.pop(p):
+                if p1 not in cluster:
+                    cluster.add(p1)
+                    queue.append(p1)
+        return cluster
 
+    _, p1, p2 = edges[0]
+
+    root_cluster = set(heapq.heappop(edges)[1:])
     while edges:
-        dist, p1, p2 = heapq.heappop(edges)
-        if p1 not in cluster_map and p2 not in cluster_map:
-            cluster_no = len(cluster_map)
-            cluster_map[p1] = cluster_map[p2] = cluster_no
-            clusters[cluster_no] = {p1, p2}
-            n_clusters += 1
-        elif p1 not in cluster_map:
-            cluster_map[p1] = cluster_map[p2]
-            clusters[cluster_map[p2]].add(p1)
-        elif p2 not in cluster_map:
-            cluster_map[p2] = cluster_map[p1]
-            clusters[cluster_map[p1]].add(p2)
+        _, p1, p2 = heapq.heappop(edges)
+        if p1 in root_cluster and p2 in root_cluster:
+            continue
+        elif p1 in root_cluster:
+            root_cluster.update(get_cluster(p2))
+        elif p2 in root_cluster:
+            root_cluster.update(get_cluster(p1))
         else:
-            cluster1, cluster2 = clusters[cluster_map[p1]], clusters[cluster_map[p2]]
-            if cluster1 is not cluster2:
-                cluster_no = cluster_map[p1]
-                cluster1.update(cluster2)
-                for p in cluster2:
-                    cluster_map[p] = cluster_no
-                n_clusters -= 1
-        if n_clusters == 1 and len(cluster_map) == 1000:
+            adjacent_map[p1].append(p2)
+            adjacent_map[p2].append(p1)
+        if len(root_cluster) == len(points):
             return p1[0] * p2[0]
     raise AssertionError("oops")
 
